ff5e63ac1499941099c399ded861a5c18cbc81f6
chenyanzhong <chen_yanzhong@outlook.com>
2018-08-07 15:28:03 +0800

    增加:增加webpack插件整理

--- webpack-demo1/build/webpack.base.conf.js
+++ webpack-demo1/build/webpack.base.conf.js
@@ -1,7 +1,8 @@
 import path from 'path'
 
+const isTest = true;
+
 module.exports = {
-    mode: 'development',
     entry: {
         main: path.resolve(__dirname, './../src/main.js')
     },
@@ -9,9 +10,16 @@ module.exports = {
         path: path.resolve(__dirname, './../src'),
         filename: '[name]-bundle.js'
     },
+    plugins: [
+
+    ],
     /**
      * 通过在浏览器调试工具(browser devtools)中添加元信息(meta info)增强调试
      * 牺牲了构建速度的 `source-map' 是最详细的。
      */
-    devtool: 'source-maps',
+    devtool: true ? 'source-maps' : 'eval',
+    mode: true ? 'development' : '',
+    devServer: {
+        hot: true
+    },
 };
